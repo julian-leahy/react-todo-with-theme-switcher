803f2ed1f88e86cb5cd00adf513f95f640916374
julian-leahy <julian.leahy.dev@gmail.com>
2021-05-11 12:49:18 +1200

    add new todo item with unique id

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { useState } from 'react';
 import './App.css';
 import FilterBtn from './components/filter-btn/FilterBtn';
 import FormInput from './components/form-input/FormInput';
 import Header from './components/header/Header';
 import ListFooter from './components/list-footer/ListFooter';
 import TodoItem from './components/todo-item/TodoItem';
+import { nanoid } from 'nanoid';
 
 function App({ data }) {
-  const tasks = data.map(({ id, name, completed }) => <TodoItem key={id} id={id} name={name} completed={completed} />)
+
+  const [taskList, setTaskList] = useState(data || []);
+
+  const newTask = (name) => {
+    const task = {
+      id: `todo-${nanoid()}`,
+      name,
+      completed: false
+    }
+    setTaskList([...taskList, task])
+  }
+
+  const tasks = taskList.map(({ id, name, completed }) => (
+    <TodoItem
+      key={id}
+      id={id}
+      name={name}
+      completed={completed} />));
+
   return (
     <div className="App">
       <div className="hero"></div>
       <div className="container">
         <Header />
         <div className="todo">
-          <FormInput />
+          <FormInput newTask={newTask} />
         </div>
         <ul className="todo-list">
           {tasks}
--- src/components/form-input/FormInput.js
+++ src/components/form-input/FormInput.js
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import './FormInput.css';
 
-function FormInput() {
+function FormInput({ newTask }) {
+
+    const [todoText, setTodoText] = useState('');
+
+    const handleChange = (e) => {
+        setTodoText(e.target.value);
+    }
+
+    const submitTodo = (e) => {
+        e.preventDefault();
+        if (todoText.length === 0) return;
+        newTask(todoText);
+        setTodoText('');
+    }
+
     return (
-        <form>
+        <form onSubmit={submitTodo}>
             <input
                 type="text"
                 aria-label="Create a new todo item"
                 className="input__lg item"
-                placeholder="Create a new todo..." />
+                placeholder="Create a new todo..."
+                value={todoText}
+                onChange={handleChange} />
             <button className="btn btn__add" type="submit">Add</button>
         </form>
     )
